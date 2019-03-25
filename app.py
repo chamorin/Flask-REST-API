e99c0f220a037215790cdb50a150bdc12d89be33
Charles Morin <charles.mor@outlook.fr>
2019-03-24 21:56:21 -0400

    Added jwt callbacks

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import hashlib
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, UserLogin, User
+from resources.user import UserRegister, UserLogin, User, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -24,13 +24,55 @@ def create_tables():
 jwt = JWTManager(app)
 app.config['JWT_AUTH_HEADER_PREFIX'] = 'Bearer'
 
+
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     identity = str(identity).encode()
-    print(hashlib.sha1(identity).hexdigest())
-    if hashlib.sha1(identity).hexdigest() == '356a192b7913b04c54574d18c28d46e6395428ab':    # Not considered safe, testing purposes only : should be in a config file
+    # Not considered safe, testing purposes only : should be in a config file
+    if hashlib.sha1(identity).hexdigest() == '356a192b7913b04c54574d18c28d46e6395428ab':
         return {'is_admin': True}
     return {'is_admin': False}
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(error):
+    return jsonify({
+        'description': 'Token is not fresh',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(error):
+    return jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'Token expired',
+        'error': 'token_expired'
+    }), 401
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -39,6 +81,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 
 if __name__ == '__main__':          # Prevents app from running multiple time
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,10 @@
 from flask_restful import Resource, Api, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import (
+    jwt_required, jwt_optional, 
+    get_jwt_claims, 
+    get_jwt_identity, 
+    fresh_jwt_required
+)
 
 from models.item import ItemModel
 
@@ -23,7 +28,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'Item not found'}, 404
 
-    @jwt_required
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with the name '{}' already exists".format(name)}, 400
@@ -65,6 +70,15 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-
+    
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.query.all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'Login for more data'
+        }, 200
